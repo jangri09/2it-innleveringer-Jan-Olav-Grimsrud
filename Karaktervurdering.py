a33e61fa582f58cf1ec272ccf423393658e69e57
jangri09 <jangri09@gvgs.no>
2026-09-04 12:35:37 +0200

    feil vurderinger

--- Karaktervurdering.py
+++ Karaktervurdering.py
@@ -1,10 +1,10 @@
 poeng = int(input("Skriv inn poengene dine: "))
 
-if poeng <= 90:
+if poeng >= 90:
     print("meget bra")
-elif poeng < 70:
+elif poeng >= 70:
     print("godt")
-elif poeng < 50:
+elif poeng >= 50:
     print("bestått")
 else:
     print("ikke bestått")
