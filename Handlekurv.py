1d5b09f7b48d8382d57db2e125ff1cc39daebad5
jangri09 <jangri09@gvgs.no>
2026-09-04 12:49:21 +0200

    lagt til kode for å regne ut totalpris og printe ut

--- Handlekurv.py
+++ Handlekurv.py
@@ -2,3 +2,11 @@ pris_energidrikk = 25
 antall_energidrikk = 2
 pris_baguette = 45
 antall_baguette = 3
+
+total_energidrikk = pris_energidrikk * antall_energidrikk
+total_baguette = pris_baguette * antall_baguette
+totalpris = total_energidrikk + total_baguette
+
+print("totalpris energidrikk:", total_energidrikk)
+print("totalpris baguette:", total_baguette)
+print("Totalpris:", totalpris)
