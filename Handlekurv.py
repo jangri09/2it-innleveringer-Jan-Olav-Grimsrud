9da2b9763117b7f85a49f577ebb41af164a11270
jangri09 <jangri09@gvgs.no>
2026-09-04 12:57:33 +0200

    lagt til at man kan velge selv antall

--- Handlekurv.py
+++ Handlekurv.py
@@ -1,7 +1,23 @@
-pris_energidrikk = 25
-antall_energidrikk = 2
 pris_baguette = 45
-antall_baguette = 3
+pris_energidrikk = 25
+
+print("Velkommen til handlekurven!")
+print("Vi har følgende varer tilgjengelig:")
+print(f"1. Energidrikk - {pris_energidrikk} kr")
+print(f"2. Baguette - {pris_baguette} kr")
+
+while True:
+    try:
+        antall_energidrikk = int(input("Hvor mange energidrikker vil du kjøpe? "))
+        break
+    except ValueError:
+        print("Vennligst skriv inn et gyldig tall for antall energidrikker.")
+while True:
+    try:
+        antall_baguette = int(input("Hvor mange baguetter vil du kjøpe? "))
+        break
+    except ValueError:
+        print("Vennligst skriv inn et gyldig tall for antall baguetter.")
 
 total_energidrikk = pris_energidrikk * antall_energidrikk
 total_baguette = pris_baguette * antall_baguette
