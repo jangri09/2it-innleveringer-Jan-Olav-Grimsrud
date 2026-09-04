10cc395075b5cf9be7465e1034b1768085d57d88
jangri09 <jangri09@gvgs.no>
2026-09-04 12:20:47 +0200

    programmet printer ut svarene

--- Personkort.py
+++ Personkort.py
@@ -2,3 +2,8 @@ navn = input("Skriv inn navnet ditt: ")
 alder = int(input("Skriv inn alderen din: "))
 høyde = float(input("Skriv inn høyden din i meter: "))
 liker_python = input("Liker du Python? (ja/nei): ").strip().lower() == "ja"
+
+print("Navn:", navn)
+print("Alder:", alder)
+print("Høyde:", høyde)
+print("Liker Python:", liker_python)
