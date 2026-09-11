eaf0c2217214b5b6d1fe35422dbfa392931f69f0
jangri09 <jangri09@gvgs.no>
2026-09-11 13:52:05 +0200

    forbedret inputvalidering for poeng i karaktervurdering; fjernet unødvendig sjekk og justert flyt

--- python_lekser/øve-oppgave-1/Karaktervurdering.py
+++ python_lekser/øve-oppgave-1/Karaktervurdering.py
@@ -1,19 +1,17 @@
-poeng = int(input("Skriv inn poengene dine: "))
-
 while True:
     try:
         poeng = int(input("Skriv inn poengene dine: "))
-        break
+        if 0 <= poeng <= 100:
+            break
+        print("Vennligst skriv inn et tall mellom 0 og 100.")
     except ValueError:
         print("Vennligst skriv inn et gyldig tall for poengene.")
 
-if poeng < 0 or poeng > 100:
-    print("Vennligst skriv inn et gyldig tall for poengene (0-100).")
-elif poeng >= 90:
-        print("meget bra")
+if poeng >= 90:
+    print("meget bra")
 elif poeng >= 70:
-        print("godt")
+    print("godt")
 elif poeng >= 50:
-        print("bestått")
+    print("bestått")
 else:
-        print("ikke bestått")
+    print("ikke bestått")
